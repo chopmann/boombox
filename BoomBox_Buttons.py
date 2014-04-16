62257d407d0fa450b0d1e3453a99e723fa8c2535
Cornelio Hopmann <hopmann@gmail.com>
2014-04-16 22:15:22 +0200

    FIX: Invalid Token

--- BoomBox_Buttons.py
+++ BoomBox_Buttons.py
@@ -1,9 +1,9 @@
 # BCM Layout
 
-PLAY     = 09 # MID
+PLAY     = 9 # MID
 STOP     = 22 # BOT
 REW      = 17 # LEFT
 FWD      = 10 # RIGHT
 MENU     = 27 # TOP
-VOL_UP   = 04 # UP
-VOL_DOWN = 02 # DOWN
+VOL_UP   = 4 # UP
+VOL_DOWN = 2 # DOWN
--- BoomBox_LCD.py
+++ BoomBox_LCD.py
@@ -1,17 +1,17 @@
 # BCM Layout
-LCD_RS   = 18
-LCD_E    = 23
-LCD_D4   = 24 
-LCD_D5   = 25
-LCD_D6   = 08
-LCD_D7   = 07
+RS   = 18
+E    = 23
+D4   = 24 
+D5   = 25
+D6   = 8
+D7   = 7
 # Define some device constants
-LCD_WIDTH = 16    # Maximum characters per line
-LCD_CHR = True
-LCD_CMD = False
+WIDTH = 16    # Maximum characters per line
+CHR = True
+CMD = False
 
-LCD_LINE_1 = 0x80 # LCD RAM address for the 1st line
-LCD_LINE_2 = 0xC0 # LCD RAM address for the 2nd line 
+LINE_1 = 0x80 # LCD RAM address for the 1st line
+LINE_2 = 0xC0 # LCD RAM address for the 2nd line 
 
 # Timing constants
 E_PULSE = 0.00005
